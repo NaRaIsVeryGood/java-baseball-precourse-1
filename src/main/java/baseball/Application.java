1d9828a8fc97b9c716599d13d243f1b1a0fed89b
NARA <NARA@121.137.62.148>
2021-10-03 09:45:24 +0900

    1. 랜덤으로 3자리 숫자 생성 2. 값 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+	
+	public static void main(String[] args) {
+
+		String randomVal = String.valueOf(Randoms.pickNumberInRange(1, 9)) + String.valueOf(Randoms.pickNumberInRange(1, 9)) + String.valueOf(Randoms.pickNumberInRange(1, 9));
+    	System.out.println("randomNumber : " + randomVal);  
+        
+    	System.out.print("숫자를 입력해주세요 : ");
+    	String inputVal = Console.readLine();
+    	
+    	System.out.println(inputVal);
+    	
     }
 }
